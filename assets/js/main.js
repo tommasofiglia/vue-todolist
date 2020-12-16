3b21b2604cf428cd35a2a8200e2b5a7532365ad5
Tommaso Figlia <tommasofiglia@gmail.com>
2020-12-16 15:07:03 +0100

    HTML e CSS. Inizio dei lavori

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,17 @@
-let app = new Vue({
+/*******************************
+Funzionalitá:
+La nostra todo list avrá alcune tasks di default predefinite
+L'utente puó inserire nuove tasks
+Cliccando sulla "X" l'utente puó cancellare una task
+Se non ci sono piu task nella lista, mostrate un messaggio tipo "Nulla da fare"
+Quando l'utente inserisce una task ha due modi per salvarla: o preme il pulsante add o preme il taso Enter della tastiera.
+Attenzione: l'utente non deve inserire tasks vuote ma almeno un tot di caratteri.
+Da tenere a mente:
+Ricordate di scrivere le istruzioni prima di iniziare a lavorare cercate di descrivere in modo sintetico gli step che pensate siano necessari.
+Consultate la documentazione e se siete bloccati, aprite un ticket.
+/*********************************
+
+let appTodo = new Vue({
   // options object
-  el: "#app",
+  el: "#appTodo",
 });
