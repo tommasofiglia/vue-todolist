d75821c4d54fc78f712f65154df6683055e764a7
Tommaso Figlia <tommasofiglia@gmail.com>
2020-12-18 01:33:10 +0100

    Bonus caricato con svuota cestino non funzionante

--- bonus/assets/js/main.js
+++ bonus/assets/js/main.js
@@ -1,14 +1,9 @@
 /*******************************
-Funzionalitá:
-La nostra todo list avrá alcune tasks di default predefinite
-L'utente puó inserire nuove tasks
-Cliccando sulla "X" l'utente puó cancellare una task
-Se non ci sono piu task nella lista, mostrate un messaggio tipo "Nulla da fare"
-Quando l'utente inserisce una task ha due modi per salvarla: o preme il pulsante add o preme il taso Enter della tastiera.
-Attenzione: l'utente non deve inserire tasks vuote ma almeno un tot di caratteri.
-Da tenere a mente:
-Ricordate di scrivere le istruzioni prima di iniziare a lavorare cercate di descrivere in modo sintetico gli step che pensate siano necessari.
-Consultate la documentazione e se siete bloccati, aprite un ticket.
+L'utente vuole poter modificare una task giá inserita
+ma vuole anche poter indicare che la task é stata completata.
+Inoltre se una task é stata completa allora vuole che venga inserita in un'altra colonna tipo "tasks completate".
+Ah non é finita, dice che quando cancella una task, non vuole che questa venga subito rimossa, ma vuole che resti visibile ma venga spostata in una colonna tipo "cestino".
+Si, l'utente é un rompi scatole, dice infine che vuole poter rimuovere tutte le tasks nel cestino cliccando su un pulsante tipo "svuota cestino".
 ********************************/
 
 let appTodo = new Vue({
@@ -16,7 +11,9 @@ let appTodo = new Vue({
   data: {
     logoImg: "assets/img/logo.png",
     newTodo: "",
-    toDoList: []
+    toDoList: [],
+    checkedList: [],
+    deletedList: []
   },
   created() {
      document.addEventListener("keyup", event => {
@@ -27,13 +24,40 @@ let appTodo = new Vue({
   },
   methods: {
 
+    // Funzione che aggiunge la task scritta nell'input
     addNew: function (){
-      //Uso un operatore ternario per impostare la condizione
-      (this.newTodo.length > 3) ? this.toDoList.push(this.newTodo) : alert("Hai inserito un comando troppo corto! Devi inserire un minimo di 4 caratteri") ;
+      // Uso un if per impostare la condizione
+      if (this.newTodo.length > 3) {
+        this.toDoList.push(this.newTodo);
+        this.newTodo="";
+      } else {
+        alert("Hai inserito un comando troppo corto! Devi inserire un minimo di 4 caratteri");
+      }
     },
 
+    // Funzione che aggiunge la task al cestino cancellandola dalla to do list
     removeTask: function(index) {
+      this.deletedList.push(this.toDoList[index]);
       this.toDoList.splice(index, 1);
+      console.log(this.deletedList);
+    },
+
+    // Funzione che ripristina una task dal cestino alla to do list
+    redoTasks: function(index) {
+      this.toDoList.push(this.deletedList[index]);
+      this.deletedList.splice(index, 1);
+    },
+
+    // Funzione che aggiunge la task alla lista delle task completate
+    taskDone: function(index){
+      this.checkedList.push(this.toDoList[index]);
+      this.toDoList.splice(index, 1);
+    },
+
+    // Funzione che svuota il Cestino
+    removeAll: function(){
+      this.deletedList.length = 0;
+      console.log(this.deletedList);
     }
   }
 
